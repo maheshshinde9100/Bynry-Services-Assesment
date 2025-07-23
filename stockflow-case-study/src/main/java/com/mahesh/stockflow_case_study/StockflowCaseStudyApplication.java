9b47868f1d9170ba882a712773c1b4999c634a57
MAHESH <maheshshinde9100@gmail.com>
2025-07-23 19:58:02 +0530

    intial commit

---
+++ stockflow-case-study/src/main/java/com/mahesh/stockflow_case_study/StockflowCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.stockflow_case_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockflowCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockflowCaseStudyApplication.class, args);
+	}
+
+}
---
+++ stockflow-case-study/src/test/java/com/mahesh/stockflow_case_study/StockflowCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.stockflow_case_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockflowCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
